622fba27a041efdd9d773f9f2614eedc6b6095f0
JoseACipriani <joseacipriani@gmail.com>
2024-06-03 23:17:43 -0300

    feito instalado do react e feito primeiras alterações

---
+++ projeto-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Fala tu mano REACT</h2>
+      <p>Hello world do react</p>
+    </div>
+  );
+}
+
+export default App;
